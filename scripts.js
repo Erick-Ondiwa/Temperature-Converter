3b2b4e065510b021ef73e65f8873b81af6bd5928
erick-ondiwa <erickochieng3860@gmail.com>
2024-06-03 23:21:21 +0300

    First modification

--- scripts.js
+++ scripts.js
@@ -1,16 +1,34 @@
 const display_area = document.getElementById('display');
-const temp = display_area.value;
-let input = Number(temp);
-
 
 const c_to_f = document.getElementById('c_to_f');
 const f_to_c = document.getElementById('f_to_c');
 
+function notANumber(temp){
+  if(isNaN(temp)){
+    display_area.value = "";
+    alert(`Please enter a valid number. ${temp} is not a number.`);
+   
+    return;
+  }
+}
+
+let input;
 function compte_result(result){
   if(c_to_f.checked){
+    const temp = display_area.value;
+   notANumber(temp);
+ 
+    let input = Number(temp);
     result = (input * 9/5) + 32;
   }
   else if(f_to_c.checked){
+    const temp = display_area.value;
+    if(isNaN(temp)){
+      alert(`Please enter a valid number. ${temp} is not a number.`);
+      converted = "";
+      return;
+    }
+    let input = Number(temp);
     result = (input - 32) * 5/9;
   }
 
@@ -20,7 +38,6 @@ function compte_result(result){
 
   return result;
 }
-
 
 const convert_button = document.getElementById('convert_button');
 convert_button.addEventListener('click', () =>{
